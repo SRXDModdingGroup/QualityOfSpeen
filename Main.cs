a630ed5a7b45f27b47245c120dbeffee4eb1b680
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2021-10-04 18:14:32 +0200

    Update Mod Version for Release

--- Main.cs
+++ Main.cs
@@ -13,7 +13,7 @@ namespace QualityOfSpeen
         #region Mod Metadata
         public const string MOD_ID = "QualityOfSpeen";
         public const string MOD_NAME = "Quality of Speen";
-        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_VERSION = "1.0.0";
         #endregion
 
         #region Mod Variables
