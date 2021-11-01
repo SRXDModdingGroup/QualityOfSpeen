c703ba6878184387e02f9bc04b9111cdc0346a1b
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2021-11-01 15:59:39 +0100

    Added No Intro Feature

---
+++ Features/NoIntro.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace QualityOfSpeen.Features
+{
+    public class NoIntro
+    {
+        private static bool SkipIntro;
+
+        private static void Awake()
+        {
+            SkipIntro = Main.ModConfig.GetValueOrDefaultTo("NoIntro", "AutoSkip", false);
+        }
+
+        [HarmonyPatch(typeof(StartupScene), nameof(StartupScene.Update))]
+        [HarmonyPostfix]
+        private static void StartupSceneAwakePostfix(StartupScene __instance)
+        {
+            if (Input.anyKeyDown) SkipIntro = true;
+            if (SkipIntro)
+            {
+                __instance._animationTimer = 8f;
+            }
+        }
+    }
+}
